251ca4ddaba9de827ff6d7f9ffcd5e54a901067e
Jakub Smykal <smykal0jakub@gmail.com>
2021-02-14 11:20:42 +0100

    set my own keybindings

--- config.def.h
+++ config.def.h
@@ -1,10 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 7;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -19,7 +25,12 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "7", "8", "9" };
+
+static const char browser[] = "firefox";
+static const char terminal[] = "kitty";
+static const char fileexp[] = "dolphin";
+static const char mail[] = "Thunderbird";
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,7 +39,12 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ browser,  NULL,       NULL,       1,            0,           -1 },
+	{ terminal,    NULL,       NULL,       1 << 1,            0,           -1 },
+	{ fileexp,    NULL,       NULL,       1 << 2,            0,           -1 },
+	{ mail,    NULL,       NULL,       1 << 5,            0,           -1 },
+	{ "Spotify",    NULL,       NULL,       1 << 4,            0,           -1 },
+	{ "electronplayer",    NULL,       NULL,       1 << 4,            0,           -1 },
 };
 
 /* layout(s) */
@@ -44,7 +60,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,31 +73,60 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+
+    // Spawning apps
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd} },
+	{ MODKEY,                       XK_e,      spawn,          {.v = fileexp} },
+
+    // Brightness control
+    { 0,                            XF86XK_MonBrightnessUp, spawn, SHCMD("brightness up") }, 
+    { 0,                            XF86XK_MonBrightnessDown, spawn, SHCMD("brightness down") }, 
+
+    // Window  movement
+	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
+ 	//{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = +1 } },
+ 	//{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+    
+    // Move window to master
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+        
+    // Switch between last two viewed tags
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+
+    // Kill window
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+
+    // Layouts
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    
+    // Switch between last two used layouts
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+
+	{ MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
+
+    // View all tags
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+
+    // Show window in all tags
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+
+    // Switch monitor focus
+	{ MODKEY|ControlMask,           XK_j,  focusmon,           {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_k,  focusmon,           {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
@@ -93,7 +138,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+    // Exit dwm
+	{ MODKEY|ControlMask,           XK_q,      quit,           {0} },
+
+    // Restart dwm
+	//{ MODKEY|ControlMask,           XK_r,      quit,           {1} },
 };
 
 /* button definitions */
@@ -102,14 +152,27 @@ static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+
+    // Middle mouse button on window title - move to master
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+
+    // Click Status text to spawn a terminal
+//  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+
+    // Super + left click to drag a window, S + right click to resize a window
+    // Super + right click to toggle floating
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+
+    // Left click to view, Right click to view multiple tags
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+
+    // Super + left click - move current window to selected tag
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+
+    // Super + right click - display ("clone") current window in another tag
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
